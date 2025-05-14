3bb50533342b2894904bc5264050b1a8a13c54fc
Zenbook <Zenbook@ASUS>
2025-05-14 15:14:55 +0200

    vgbhnj

---
+++ Be-better-app___---/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Be_better_app______
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
